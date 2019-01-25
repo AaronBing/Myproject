b68431190d100a252ca344bb875b36eed9b43286
AaronBing <15626187207@163.com>
2019-01-25 22:51:43 +0800

    bldc code descrition

--- Learn/BLDC/01STMC/STSPIN/stsw3202BLDC/en.stsw-spin3202/STSW-SPIN3202-V1.0.0/stm32_cube/Middlewares/ST/MC_6Step_Lib/Inc/6Step_Lib.h
+++ Learn/BLDC/01STMC/STSPIN/stsw3202BLDC/en.stsw-spin3202/STSW-SPIN3202-V1.0.0/stm32_cube/Middlewares/ST/MC_6Step_Lib/Inc/6Step_Lib.h
@@ -94,33 +94,33 @@ typedef enum
   */
 typedef struct
 {
-  uint32_t LF_TIMx_PSC;                  /*!< Prescaler variable for low frequency timer */
-  uint32_t LF_TIMx_ARR;                  /*!< ARR variable for low frequency timer */
-  uint32_t HF_TIMx_PSC;                  /*!< Prescaler variable for high frequency timer */
-  uint32_t HF_TIMx_ARR;                  /*!< ARR variable for high frequency timer */
-  uint16_t pulse_value;                  /*!< Gate driver timer pulse value for SixStep algorithm */
+  uint32_t LF_TIMx_PSC;                  /*!< Prescaler variable for low frequency timer 预分频器变量用于低频定时器*/
+  uint32_t LF_TIMx_ARR;                  /*!< ARR variable for low frequency timer 用于低频定时器的ARP变量*/
+  uint32_t HF_TIMx_PSC;                  /*!< Prescaler variable for high frequency timer 预分频变量用于高频定时器*/
+  uint32_t HF_TIMx_ARR;                  /*!< ARR variable for high frequency timer 用于高频定时器的ARP变量*/
+  uint16_t pulse_value;                  /*!< Gate driver timer pulse value for SixStep algorithm栅极驱动器定时器脉冲值 */
 #ifndef VOLTAGE_MODE
-  uint16_t current_reference;            /*!< Current reference for SixStep algorithm*/  
+  uint16_t current_reference;            /*!< Current reference for SixStep algorithm 参考电流*/  
 #endif
-  uint16_t startup_reference;            /*!< Startup value for pulse value or current reference for SixStep algorithm*/   
-  uint8_t step_position;                 /*!< Step number variable for SixStep algorithm */
-  SIXSTEP_Base_SystStatus_t STATUS;      /*!< Status variable for SixStep algorithm */  
+  uint16_t startup_reference;            /*!< Startup value for pulse value or current reference for SixStep algorithm脉冲值或电流参考的启动值*/   
+  uint8_t step_position;                 /*!< Step number variable for SixStep algorithm 步数变量*/
+  SIXSTEP_Base_SystStatus_t STATUS;      /*!< Status variable for SixStep algorithm 状态变量*/  
 #ifdef HALL_SENSORS
-  uint8_t  next_step_pos;                /*!< Next step position for SixStep algorithm */
-  uint16_t hall_capture;                 /*!< Input capture register value when hall status changes */
-  uint16_t commutation_delay;            /*!< Delay between hall capture and step commutation */
-  uint8_t hall_ok;                       /*!< Indication of hall capture */
-  uint8_t hall_ko_successive;             /*!< Number of successive no hall capture before timer expiration */
-  uint8_t start_attempts;                /*!< Number of start attempts */
-  uint8_t run_attempts;                  /*!< Number of run attempts */
-  volatile int32_t start_cnt;            /*!< Counter used to compute initial commutation delays */
+  uint8_t  next_step_pos;                /*!< Next step position for SixStep algorithm 下一步位置*/
+  uint16_t hall_capture;                 /*!< Input capture register value when hall status changes Hall状态改变时输入补货寄存器值*/
+  uint16_t commutation_delay;            /*!< Delay between hall capture and step commutation Hall捕获和阶梯换向之间的延迟*/
+  uint8_t hall_ok;                       /*!< Indication of hall capture Hall捕获的指示*/
+  uint8_t hall_ko_successive;             /*!< Number of successive no hall capture before timer expiration 定时器到期之前没有hall捕获的数量*/
+  uint8_t start_attempts;                /*!< Number of start attempts 启动尝试次数*/
+  uint8_t run_attempts;                  /*!< Number of run attempts 运行尝试次数*/
+  volatile int32_t start_cnt;            /*!< Counter used to compute initial commutation delays 用于计算初始换向延迟的计数器*/
 #else
-  uint8_t prev_step_pos;                 /*!< Previous step position for SixStep algorithm */
-  uint16_t numberofitemArr;              /*!< Number of elements for motor startup */
-  uint8_t ADC_BEMF_channel[3];           /*!< Array of ADC regular channels used for BEMF sensing */
-  uint8_t Current_ADC_BEMF_channel;      /*!< ADC regular channel to select for BEMF sensing */  
-  uint16_t ADC_BEMF_Buffer[7];           /*!< Buffer for ADC regular channels used for BEMF sensing */
-  uint16_t ADC_BEMF_threshold_UP;        /*!< Voltage threshold for BEMF detection in up direction during OFF time*/  
+  uint8_t prev_step_pos;                 /*!< Previous step position for SixStep algorithm 上一步位置*/
+  uint16_t numberofitemArr;              /*!< Number of elements for motor startup 电机启动的元件数量*/
+  uint8_t ADC_BEMF_channel[3];           /*!< Array of ADC regular channels used for BEMF sensing 用于BEMF传感的ADC常规通道阵列*/
+  uint8_t Current_ADC_BEMF_channel;      /*!< ADC regular channel to select for BEMF sensing ADC常规通道选择BEMF传感*/  
+  uint16_t ADC_BEMF_Buffer[7];           /*!< Buffer for ADC regular channels used for BEMF sensing 缓冲器用于BEMF传感*/
+  uint16_t ADC_BEMF_threshold_UP;        /*!< Voltage threshold for BEMF detection in up direction during OFF time 电压阈值检测BEMF向上*/  
   uint16_t ADC_BEMF_threshold_DOWN;      /*!< Voltage threshold for BEMF detection in down direction during OFF time*/
 #ifdef PWM_ON_BEMF_SENSING
   uint16_t ADC_BEMF_threshold_UP_ON;     /*!< Voltage threshold for BEMF detection in up direction during ON time*/  
@@ -128,30 +128,30 @@ typedef struct
   uint16_t ADC_Current_BEMF_thld_UP;     /*!< Voltage threshold for BEMF detection in up direction*/  
   uint16_t ADC_Current_BEMF_thld_DOWN;   /*!< Voltage threshold for BEMF detection in down direction*/
 #endif
-  uint16_t demagn_counter;               /*!< Demagnetization counter */
-  uint16_t demagn_value;                 /*!< Demagnetization value */  
+  uint16_t demagn_counter;               /*!< Demagnetization counter 退磁计数器*/
+  uint16_t demagn_value;                 /*!< Demagnetization value 退磁价值*/  
 #endif
   uint8_t ADC_SEQ_Channel[4];            /*!< Array of ADC channels used for speed, current, vbus or temperature sensing */
-  uint16_t ADC_SEQ_Buffer[4];            /*!< Buffer for ADC regular channels used for non BEMF measurements */
-  int32_t speed_fdbk;                    /*!< Motor speed variable */  
-  int32_t speed_fdbk_filtered;           /*!< Filtered Motor speed variable */
-  int32_t speed_target;                  /*!< Speed target for the motor */
+  uint16_t ADC_SEQ_Buffer[4];            /*!< Buffer for ADC regular channels used for non BEMF measurements 非BEMF测量的ADC常规通道*/
+  int32_t speed_fdbk;                    /*!< Motor speed variable 电机速度变量*/  
+  int32_t speed_fdbk_filtered;           /*!< Filtered Motor speed variable 过滤电机速度变量*/
+  int32_t speed_target;                  /*!< Speed target for the motor 电机的速度目标*/
 
 #ifdef PID
   int32_t error2;
   int32_t error1;
 #else
-  int32_t Integral_Term_sum;             /*!< Global Integral part for PI*/
+  int32_t Integral_Term_sum;             /*!< Global Integral part for PI 全局整体部分*/
 #endif
-  uint8_t ALIGN_OK;                      /*!< Flag control for Motor Alignment*/ 
-  uint8_t ALIGNMENT;                     /*!< Flag control for Motor Alignment ongoing*/   
-  uint16_t Speed_Loop_Time;              /*!< Speed loop variable for timing */ 
+  uint8_t ALIGN_OK;                      /*!< Flag control for Motor Alignment电机对准*/ 
+  uint8_t ALIGNMENT;                     /*!< Flag control for Motor Alignment ongoing正在进行电机对准*/   
+  uint16_t Speed_Loop_Time;              /*!< Speed loop variable for timing 速度循环变量计时*/ 
 
   uint16_t RUN_Motor;                    /*!< Flag for Motor status */ 
   uint8_t ARR_OK;                        /*!< ARR flag control for Accell status */ 
-  uint8_t VALIDATION_OK;                 /*!< Validation flag for Closed loop control begin */ 
-  uint8_t SPEED_VALIDATED;               /*!< Validation flag for Speed before closed loop control */ 
-  uint8_t TARGET_SPEED_SWITCHED;         /*!< Target speed has been switched to final speed in closed loop */
+  uint8_t VALIDATION_OK;                 /*!< Validation flag for Closed loop control begin 闭环控制开始*/ 
+  uint8_t SPEED_VALIDATED;               /*!< Validation flag for Speed before closed loop control 闭环前速度验*/ 
+  uint8_t TARGET_SPEED_SWITCHED;         /*!< Target speed has been switched to final speed in closed loop 目标速度在闭环中切换到最终速度*/
   
   
   uint32_t SYSCLK_frequency;             /*!< System clock main frequency */ 
@@ -170,16 +170,16 @@ typedef struct
   uint8_t Button_ready;                  /*!<  */
   uint8_t BEMF_OK;                       /*!<  */
   uint8_t CL_READY;                      /*!<  */
-  uint8_t BEMF_Tdown_count;              /*!< BEMF Consecutive Threshold Falling Crossings Counter */   
-  uint16_t IREFERENCE;                   /*!< Currrent reference*/ 
-  uint16_t NUMPOLESPAIRS;                /*!< Number of motor pole pairs  */ 
-  uint32_t ACCEL;                        /*!< Acceleration start-up parameter*/ 
+  uint8_t BEMF_Tdown_count;              /*!< BEMF Consecutive Threshold Falling Crossings Counter BEMF连续阈值下降交叉口计数器*/   
+  uint16_t IREFERENCE;                   /*!< Currrent reference */ 
+  uint16_t NUMPOLESPAIRS;                /*!< Number of motor pole pairs  电机极对数*/ 
+  uint32_t ACCEL;                        /*!< Acceleration start-up parameter 电机启动参数*/ 
   uint16_t KP;                           /*!< KP parameter for PI(D) regulator */ 
   uint16_t KI;                           /*!< KI parameter for PI(D) regulator */ 
 #ifdef PID
   uint16_t KD;                           /*!< KD parameter for PID regulator */
 #endif
-  uint8_t CW_CCW;                        /*!< Set the motor direction */ 
+  uint8_t CW_CCW;                        /*!< Set the motor direction 电机方向*/ 
   uint8_t overcurrent;
 }  SIXSTEP_Base_InitTypeDef;             /*!< Six Step Data Structure */
 
@@ -196,7 +196,7 @@ typedef struct
 
 typedef struct
 {
-  int32_t Reference;                    /*!< Current reference value for PI regulator */
+  int32_t Reference;                    /*!< Current reference value for PI regulator pi调节器的当前参考值*/
   uint16_t Kp_Gain;                     /*!< Kp value for PI(D) regulator */ 
   uint16_t Ki_Gain;                     /*!< Ki value for PI(D) regulator */
 #ifdef PID
---
+++ Learn/BLDC/01STMC/STSPIN/stsw3202BLDC/en.stsw-spin3202/STSW-SPIN3202-V1.0.0/stm32_cube/Projects/Multi/Examples/MotionControl/STEVAL-SPIN3202/MDK-ARM/STSPIN32F0/Objects/startup_stm32f031x6.d
@@ -0,0 +1,1 @@
+.\objects\startup_stm32f031x6.o: RTE\Device\STM32F031C6\startup_stm32f031x6.s
